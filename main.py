3125422e4462940568d9c70ead943db17e4006a9
benj-chen <benjchen06@gmail.com>
2023-12-21 23:11:10 -0800

    streamlined further, changed functionality (no spaces between outer letters)

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-central = input("central letter: ")
-letters = set(input("six outer letters, separated by spaces: ").split())
+central = input("central letter, e.g., l: ")
+letters = set(input("six outer letters, e.g. vitroc: "))
 letters.add(central)
 
 # read wordlist
 # must be 4 letters long and have central in it, and have only "letters"
 words = []
 with open("wordlist.txt","r") as file:
-    words = [word.strip() for word in file.readlines()]
-
-words = [word for word in words if len(word)>3 and 
-        central in word and
-        len(set(word) - letters)==0
-        ]
-
-print("\n".join(sorted(words, key=len)))
+    print("\n".join(sorted(
+                    [word for word in file.read().split("\n") if len(word)>3 and # must have 4+ letters
+                            central in word and # must contain the central letter
+                            len(set(word) - letters)==0 # all letters in word must appear in letters
+                            ], key=len # sort matching words by length
+                    ))
+         )
