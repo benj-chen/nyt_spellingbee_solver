37433eecfcec7222f0ddffdd44c58cbdddc9df09
benj-chen <benjchen06@gmail.com>
2023-12-21 23:05:52 -0800

    made it easier to read, possibly faster (definitely not slower)

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 central = input("central letter: ")
-outer = input("six outer letters, separated by spaces: ").split()
+letters = set(input("six outer letters, separated by spaces: ").split())
+letters.add(central)
 
 # read wordlist
+# must be 4 letters long and have central in it, and have only "letters"
 words = []
 with open("wordlist.txt","r") as file:
     words = [word.strip() for word in file.readlines()]
 
-# find all words, then sort by length of word
+words = [word for word in words if len(word)>3 and 
+        central in word and
+        len(set(word) - letters)==0
+        ]
 
-# must be 4 letters or longer
-ok_words = []
-for word in words:
-    if len(word)<4:
-        continue
-    if central not in word:
-        continue
-    ok = True
-    for letter in word: # there must be a more elegant way to do this.
-        if letter not in [central]+outer:
-            ok = False
-            break
-    if ok:
-        ok_words.append(word)
-
-print("\n".join(sorted(ok_words,reverse=True, key=len)))
+print("\n".join(sorted(words, key=len)))
